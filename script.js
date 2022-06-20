78fe84fec528fde1794bf9e89e32b42750378528
ANQIII123 <liu.anqi@trentglobal.edu.sg>
2022-06-20 12:25:04 +0000

    change icon

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ window.addEventListener('DOMContentLoaded', async function () {
   
       const { coords: { latitude, longitude } } = position;
       var locationIcon =  L.icon({
-          iconUrl: 'location-icons.png',
-          iconSize: [38, 50], 
+          iconUrl: 'location-icon.png',
+          iconSize: [52, 60], 
       })
       var locationMarker = new L.marker([latitude, longitude],{icon:locationIcon}).addTo(map);
 
